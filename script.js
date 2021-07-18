a5c5b301cf6d67510267efbea1a5da229091d343
Sufiyan Chaudhary <75023910+Imsufy@users.noreply.github.com>
2021-07-18 22:03:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Username"&&b=="Password")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
